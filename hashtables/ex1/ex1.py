12a219f5b396c02993ea1de6b59d2dc6ac0f9e52
Michael Harris <mhguitar1997@gmail.com>
2019-12-06 11:21:23 -0800

    EX 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,16 +13,34 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    index = 0
-    for weight in weights:
-        hash_table.insert(ht, weight, index)
-        index += 1
+    # index = 0
+    for i in range(len(weights)):
+        hash_table_insert(ht, weights[i], i)
 
-    return None
+
+        # What other weight do we need to match the limit?
+
+    for i in range(len(weights)):
+        other_weight = limit - weights[i]
+        if hash_table_retrieve(ht, other_weight):
+            # Find higher value
+            weight_index = hash_table_retrieve(ht, other_weight)
+            print("tuple", hash_table_retrieve(ht, other_weight), i)
+            return (weight_index, i)
+            # if weight_index > hash_table_retrieve(ht, weight):
+            #     return (weight_index, hash_table_retrieve(ht, weight))
+            # else:
+            #     print("Other",hash_table_retrieve(ht, weight), weight_index )
+            #     return (hash_table_retrieve(ht, weight), weight_index)
+        # else:
+        #     return None
+
+
 
 
 def print_answer(answer):
     if answer is not None:
         print(str(answer[0] + " " + answer[1]))
+        return answer[0], answer[1]
     else:
         print("None")
